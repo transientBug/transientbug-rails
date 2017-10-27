40fec5ea972ad449b2ba1f7d1510b8b2bf1c43d9
JoshAshby <joshuaashby@joshashby.com>
2017-10-27 14:59:24 -0600

    db things

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_10_27_035601) do
+ActiveRecord::Schema.define(version: 2017_10_27_184932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,16 @@ ActiveRecord::Schema.define(version: 2017_10_27_035601) do
     t.index ["user_id"], name: "index_authorizations_on_user_id"
   end
 
+  create_table "identities", force: :cascade do |t|
+    t.bigint "user_id"
+    t.text "email"
+    t.text "name"
+    t.text "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_identities_on_user_id"
+  end
+
   create_table "images", force: :cascade do |t|
     t.bigint "user_id"
     t.text "title"
@@ -91,6 +101,7 @@ ActiveRecord::Schema.define(version: 2017_10_27_035601) do
   end
 
   add_foreign_key "authorizations", "users"
+  add_foreign_key "identities", "users"
   add_foreign_key "images", "users"
   add_foreign_key "questionnaires", "users"
   add_foreign_key "questions", "questionnaires"
