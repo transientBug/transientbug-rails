34f12bc4a36e04415486edfd9cee2eeff41233e0
JoshAshby <joshuaashby@joshashby.com>
2018-01-17 09:16:01 -0700

    don't need to see others bookmarks in non-admin view

--- app/policies/bookmark_policy.rb
+++ app/policies/bookmark_policy.rb
@@ -2,22 +2,14 @@ class BookmarkPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       # return scope.where(public: true) unless user.present?
-
-      return scope.all if user.role? :admin
+      # return scope.all if user.role? :admin
 
       scope.where(user: user).order(:created_at)
     end
   end
 
   def show?
-    return false unless user.present?
-    # return record.public? unless user.present?
-
-    return true if user.role? :admin
-
-    # return record.public? unless user.owner_of? record
-
-    true
+    user&.owner_of? record
   end
 
   def permitted_attributes
