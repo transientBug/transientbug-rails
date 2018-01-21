c947df4da418a728c047f6a0b5fe72c57baf22d7
JoshAshby <joshuaashby@joshashby.com>
2018-01-20 23:03:21 -0700

    update cors and add csp for the hell of it

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,4 +5,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: %i(get post put patch delete options head)
   end
+
+  allow do
+    origins "localhost:3000", "https://transientbug.ninja"
+    resource "/*",
+      headers: :any,
+      methods: %i(get post put patch delete options head)
+  end
 end
