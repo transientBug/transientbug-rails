eeda788ee91021502d13749a7c3ab72d4c1d69af
JoshAshby <joshuaashby@joshashby.com>
2018-01-20 23:33:44 -0700

    does this fix the issue in the autumn moon yaml?

--- config/initializers/autumn_moon.rb
+++ config/initializers/autumn_moon.rb
@@ -1,6 +1,6 @@
 require "autumn_moon"
 require "transient_bug_bot"
 
-Rails.application.config_for(:autumn_moon).deep_symbolize_keys.each do |name, data|
-  AutumnMoon.register_bot data[:token], klass: data[:klass].constantize
-end
+# Rails.application.config_for(:autumn_moon).deep_symbolize_keys.each do |name, data|
+  # AutumnMoon.register_bot data[:token], klass: data[:klass].constantize
+# end
