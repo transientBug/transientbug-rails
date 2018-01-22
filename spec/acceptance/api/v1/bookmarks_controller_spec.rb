735a157609aceb95a86381bd6c28f0b16eebb981
JoshAshby <joshuaashby@joshashby.com>
2018-01-21 19:48:14 -0700

    add updating a record acceptance test and I didn't like the apitome parameters

--- spec/acceptance/api/v1/bookmarks_controller_spec.rb
+++ spec/acceptance/api/v1/bookmarks_controller_spec.rb
@@ -44,11 +44,16 @@ resource "v1 Bookmarks" do
   end
 
   post "/api/v1/bookmarks" do
-    parameter :type, required: true, scope: [ :data ]
-    parameter :url, required: true, scope: [ :data, :attributes ]
-    parameter :title, required: true, scope: [ :data, :attributes ]
-    parameter :description, scope: [ :data, :attributes ]
-    parameter :tags, scope: [ :data, :attributes ]
+    with_options scope: :data do
+      parameter :type, "bookmark", required: true
+    end
+
+    with_options scope: [ :data, :attributes ] do
+      parameter :url, "Bookmarks URL", required: true
+      parameter :title, "Title", required: true
+      parameter :description, "Description/Note"
+      parameter :tags, "Comma separated"
+    end
 
     let(:type) { "bookmark" }
     let(:url) { "https://bookmark.example" }
@@ -64,6 +69,38 @@ resource "v1 Bookmarks" do
       do_request
 
       expect(status).to eq(201)
+      expect(response_body).to match_response_schema("api/v1/bookmarks/show")
+    end
+  end
+
+  patch "/api/v1/bookmarks/:id" do
+    with_options scope: :data do
+      parameter :type, "bookmark", required: true
+      parameter :id, "Bookmark ID to update", required: true
+    end
+
+    with_options scope: [ :data, :attributes ] do
+      parameter :url, "Bookmarks URL", required: true
+      parameter :title, "Title", required: true
+      parameter :description, "Description/Note"
+      parameter :tags, "Comma separated"
+    end
+
+    let(:type) { "bookmark" }
+    let(:id) { bookmark.id }
+    let(:url) { "https://bookmark.example" }
+    let(:title) { "bookmark 1" }
+    let(:description) { "Something witty" }
+    let(:tags) { "tag 1, tag 2, tag 3" }
+
+    let(:raw_post) { params.to_json }
+
+    example "Patch Update" do
+      explanation "Updates a bookmark."
+
+      do_request
+
+      expect(status).to eq(200)
       expect(response_body).to match_response_schema("api/v1/bookmarks/show")
     end
   end
