6704e41c36abf70570e24a25b0b8e0308f42c88f
JoshAshby <joshuaashby@joshashby.com>
2018-01-23 08:43:56 -0700

    use ES for the existence check on bookmarks, should go quicker

--- app/controllers/api/v1/bookmarks/check_controller.rb
+++ app/controllers/api/v1/bookmarks/check_controller.rb
@@ -1,11 +1,20 @@
 class Api::V1::Bookmarks::CheckController < Api::V1Controller
   # GET /api/v1/bookmarks/check
   def index
-    webpage = Webpage.find_by(uri_string: params[:url])
-    head :not_found and return unless webpage
-
-    bookmark = webpage.bookmarks.where(user: current_user).exists?
-    head :not_found and return unless bookmark
+    head :not_found and return unless bookmark_found?
     head :found
+  end
+
+  protected
+
+  def bookmark_found?
+    BookmarksIndex.query(
+      bool: {
+        must: [
+          { term: { uri: params[:url] } },
+          { term: { user_id: current_user.id } }
+        ]
+      }
+    ).any?
   end
 end
