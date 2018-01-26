110aa32d2801702402521806e1dfd65c05cdbdb5
JoshAshby <joshuaashby@joshashby.com>
2018-01-26 10:56:22 -0700

    rubocop item

--- lib/active_record_upsert.rb
+++ lib/active_record_upsert.rb
@@ -1,9 +1,7 @@
 module ActiveRecordUpsert
   def upsert *attributes
-    begin
-      find_or_create_by(*attributes)
-    rescue ActiveRecord::RecordNotUnique, PG::UniqueViolation
-      retry # YOLO
-    end
+    find_or_create_by(*attributes)
+  rescue ActiveRecord::RecordNotUnique, PG::UniqueViolation
+    retry # YOLO
   end
 end
