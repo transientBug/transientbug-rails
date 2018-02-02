2352bea43679eaa2bc92bbcda97338e7291e654c
JoshAshby <joshuaashby@joshashby.com>
2018-02-02 14:48:53 -0700

    move a string notice inline

--- app/controllers/oauth/authorized_applications_controller.rb
+++ app/controllers/oauth/authorized_applications_controller.rb
@@ -8,7 +8,6 @@ class Oauth::AuthorizedApplicationsController < ApplicationController
   def destroy
     Doorkeeper::AccessToken.revoke_all_for params[:id], current_user
 
-    notice = "Application revoked"
-    redirect_to oauth_authorized_applications_url, notice: notice
+    redirect_to oauth_authorized_applications_url, notice: "Application revoked"
   end
 end
