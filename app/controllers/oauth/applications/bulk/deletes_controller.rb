4db9b58f4009a052baec38237e3a217468f51986
JoshAshby <joshuaashby@joshashby.com>
2018-02-05 15:16:32 -0700

    actually implement the two batch endpoints for revokes and oauth app deletes

--- app/controllers/oauth/applications/bulk/deletes_controller.rb
+++ app/controllers/oauth/applications/bulk/deletes_controller.rb
@@ -3,9 +3,23 @@ class Oauth::Applications::Bulk::DeletesController < ApplicationController
 
   # DELETE /oauth/applications/bulk/deletes
   def destroy
-    # binding.pry
-    flash[:info] = "Bulk delete applications successful"
-    render json: { heyo: "okayo" }, status: :ok
+    applications = Doorkeeper::Application.where id: bulk_params[:ids]
+
+    # I guess this should be done in a transaction, so that all of the destroys
+    # are rolled back if one goes wrong?
+    destroy_results = applications.each_with_object({}) do |application, memo|
+      memo[application.id] = application.destroy
+    end
+
+    all_good = destroy_results.values.all?
+
+    if all_good
+      flash[:info] = "Bulk delete applications successful"
+      render json: { bulk_results: destroy_results }, status: :ok
+    else
+      flash[:error] = "Some applications could not be deleted"
+      render json: { bulk_results: destroy_results }, status: :unprocessable_entity
+    end
   end
 
   protected
--- app/controllers/oauth/authorized_applications/bulk/revokes_controller.rb
+++ app/controllers/oauth/authorized_applications/bulk/revokes_controller.rb
@@ -3,9 +3,20 @@ class Oauth::AuthorizedApplications::Bulk::RevokesController < ApplicationContro
 
   # DELETE /oauth/authorized_applications/bulk/revokes
   def destroy
-    # binding.pry
-    flash[:info] = "Bulk application revoke successful"
-    render json: { heyo: "okayo" }, status: :ok
+    results = Doorkeeper::AccessToken.revoke_all_for bulk_params[:ids], current_user
+    revoke_results = results.each_with_object({}) do |access_token, memo|
+      memo[ access_token.application_id ] = access_token.revoked_at
+    end
+
+    all_good = revoke_results.values.all?
+
+    if all_good
+      flash[:info] = "Bulk revoke of applications successful"
+      render json: { bulk_results: revoke_results }, status: :ok
+    else
+      flash[:error] = "Some applications could not be revoked"
+      render json: { bulk_results: revoke_results }, status: :unprocessable_entity
+    end
   end
 
   protected
