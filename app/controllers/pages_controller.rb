963c159f8a880536f776ae87804c3670271a53ac
JoshAshby <joshuaashby@joshashby.com>
2018-02-11 19:42:18 -0700

    start tos and privacy policy page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,15 @@
 class PagesController < ApplicationController
-  require_login! except: [ :main, :faq ]
+  require_login! only: [ :home ]
 
-  before_action :login_redirect, only: [ :main ]
+  def main; end
 
-  def main
-  end
+  def faq; end
 
-  def faq
-  end
+  def privacy; end
+
+  def tos; end
 
   def home
     @recent_bookmarks = policy_scope(Bookmark).limit(15)
-  end
-
-  protected
-
-  def login_redirect
-    redirect_to home_path if signed_in?
   end
 end
