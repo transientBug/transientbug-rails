4396cf17bc73f94687ec12b76dd9b06bd33f7776
JoshAshby <joshuaashby@joshashby.com>
2018-02-27 12:52:56 -0700

    working pinboard import for their json export/backup

--- app/jobs/import_data/pinboard_job.rb
+++ app/jobs/import_data/pinboard_job.rb
@@ -11,8 +11,27 @@ class ImportData::PinboardJob < ApplicationJob
   private
 
   def parse_file
-    @nokogiri = Nokogiri::XML.parse import_data.upload.blob.download
+    json_data = JSON.parse @import_data.upload.download
 
-    binding.pry
+    json_data.each do |entry|
+      tags = entry["tags"].split(",").map(&:chomp)
+
+      Bookmark.for(@import_data.user, entry["href"]).tap do |bookmark|
+        # See also https://pinboard.in/api/#posts_add
+        #
+        # > description: Title of the item. This field is unfortunately named
+        #   'description' for backwards compatibility with the delicious API
+        #
+        # > extended: Description of the item. Called 'extended' for backwards
+        #   compatibility with delicious API
+        bookmark.title       = entry["description"]
+        bookmark.description = entry["extended"]
+
+        bookmark.tags = Tag.find_or_create_tags(tags: tags)
+      end.upsert
+    end
+
+    import_data.update complete: true
+    # TODO: Imported notification?
   end
 end
