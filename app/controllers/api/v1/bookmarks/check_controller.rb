0f2981c70607ded8dce7535199eb4745e11428d8
JoshAshby <joshuaashby@joshashby.com>
2018-06-01 12:12:30 -0600

    also 400 on missing param

--- app/controllers/api/v1/bookmarks/check_controller.rb
+++ app/controllers/api/v1/bookmarks/check_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::Bookmarks::CheckController < Api::V1Controller
   # GET /api/v1/bookmarks/check
   def index
+    return head :bad_request unless params[:url]
     return head :not_found unless bookmark_found?
 
     head :found
