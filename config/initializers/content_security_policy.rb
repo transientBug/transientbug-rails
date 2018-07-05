21351986366d5c7cad072aa8c00b8b6fd964a3eb
JoshAshby <joshuaashby@joshashby.com>
2018-07-05 08:15:49 -0600

    remove dup'd and non production CSP entries

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,9 +1,9 @@
 Rails.application.config.content_security_policy do |p|
-  p.default_src :self, :https, "http://localhost:*", "ws://localhost:*", "http://localhost:8080"
+  p.default_src :self, :https
   p.font_src    :self, :https, :data, "fonts.gstatic.com"
   p.img_src     :self, :https, :data
   p.object_src  :none
-  p.script_src  :self, :https, :unsafe_inline, :unsafe_eval, "http://localhost:*", "http://0.0.0.0:*"
+  p.script_src  :self, :https, :unsafe_inline
   p.style_src   :self, :https, :unsafe_inline, "fonts.googleapis.com"
 
   if Rails.env.development?
