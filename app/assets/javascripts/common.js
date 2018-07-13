47bd68b1f1f7ab6c32aa63b5b165a10ae27a062c
JoshAshby <joshuaashby@joshashby.com>
2018-07-13 14:28:37 -0600

    make the navbar a giant search field >:)

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -83,7 +83,7 @@ const tagDropdown = (selector, action, fields) => {
   const tagInput = $(`${ selector } > input[data-behaviour~=\"init\"]`)
   if(tagInput.length != 0) {
     const rawTags = tagInput.val()
-    const tags = rawTags.split(",").map((t) => ({ name: t, value: t, label: t, title: t, selected: true }))
+    const tags = rawTags.split(",").filter((t) => !(_.isEmpty(t) || _.isNil(t))).map((t) => ({ name: t, value: t, label: t, title: t, selected: true }))
 
     $(selector).dropdown({
       apiSettings: {
