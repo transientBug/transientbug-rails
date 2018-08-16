13967c817433ff9513dcc6238823b532cefcdd02
JoshAshby <joshuaashby@joshashby.com>
2018-08-16 16:43:20 -0600

    rubocop items

--- app/searchers/query_grammar/transformer.rb
+++ app/searchers/query_grammar/transformer.rb
@@ -33,7 +33,7 @@ module QueryGrammar
         field_data = index.fields[ field ]
         field_data ||= index.operators[ field ]
 
-        Cloaker.new(bind: self.index).cloak(clause, &field_data.parse)
+        Cloaker.new(bind: index).cloak(clause, &field_data.parse)
       end.flatten
 
       return inner.first if inner.length == 1
