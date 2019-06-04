88e1d22df501a1122b3115eff42b701694012380
JoshAshby <joshuaashby@joshashby.com>
2019-06-04 11:27:22 -0600

    add a basic stats rollup bar to the home page
    lists bookmarks and untagged counts and will link to a search
    for all untagged bookmarks when its non-0
    
    also move home page to a standalone controller

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,19 @@
+class DashboardController < ApplicationController
+  require_login!
+
+  def index
+    @stats = {
+      total_count: BookmarksIndex.query(
+        bool: {
+          must: [ { term: { user_id: current_user.id } } ]
+        }).count,
+      untagged_count: BookmarksIndex.query(
+        bool: {
+          must: [ { term: { user_id: current_user.id } } ],
+          must_not: [ { exists: { field: :tags } } ]
+        }).count
+    }
+
+    @recent_bookmarks = policy_scope(Bookmark).limit(5)
+  end
+end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
 class PagesController < ApplicationController
-  require_login! only: [ :home ]
-
   def main
-    redirect_to :home if signed_in?
+    redirect_to :dashboard if signed_in?
   end
 
   def faq; end
@@ -10,8 +8,4 @@ class PagesController < ApplicationController
   def privacy; end
 
   def tos; end
-
-  def home
-    @recent_bookmarks = policy_scope(Bookmark).limit(5)
-  end
 end
