daeefcac122345fe4e55c5ea7ceec2b4dde034fb
JoshAshby <joshuaashby@joshashby.com>
2019-07-20 15:57:55 -0600

    further update of the eslint rules to restrict typescipt stuff
    and adjust the react settings too because hashtag javascript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,20 @@
 module.exports = {
   parser: "babel-eslint",
+
   extends: [
-    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
     "plugin:prettier/recommended",
     "prettier/react",
     "prettier/standard",
-    "prettier/@typescript-eslint"
   ],
+
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
 
   plugins: ["prettier"],
 
@@ -18,48 +26,58 @@ module.exports = {
     Rails: true,
   },
 
-  rules: { },
-
   overrides: [
     {
-    files: ['**/*.ts', '**/*.tsx'],
+      files: ['**/*.ts', '**/*.tsx'],
 
-    parser: '@typescript-eslint/parser',
+      parser: '@typescript-eslint/parser',
 
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
+      extends: [
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+        "prettier/react",
+        "prettier/standard",
+        "prettier/@typescript-eslint"
+      ],
+
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+
+        // typescript-eslint specific options
+        project: "./tsconfig.json",
+        //tsconfigRootDir: projectRootPath,
+        warnOnUnsupportedTypeScriptVersion: true,
       },
 
-      // typescript-eslint specific options
-      project: "./tsconfig.json",
-      //tsconfigRootDir: projectRootPath,
-      warnOnUnsupportedTypeScriptVersion: true,
-    },
+      plugins: ['@typescript-eslint'],
 
-    plugins: ['@typescript-eslint'],
+      rules: {
+        // These ESLint rules are known to cause issues with typescript-eslint
+        // See https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/configs/recommended.json
+        camelcase: 'off',
+        indent: 'off',
+        'no-array-constructor': 'off',
+        'no-unused-vars': 'off',
 
-    rules: {
-      // These ESLint rules are known to cause issues with typescript-eslint
-      // See https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/configs/recommended.json
-      camelcase: 'off',
-      indent: 'off',
-      'no-array-constructor': 'off',
-      'no-unused-vars': 'off',
+        '@typescript-eslint/no-angle-bracket-type-assertion': 'warn',
+        '@typescript-eslint/no-array-constructor': 'warn',
+        '@typescript-eslint/no-namespace': 'error',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            args: 'none',
+            ignoreRestSiblings: true,
+          }
+        ],
+        '@typescript-eslint/explicit-function-return-type': 'off',
 
-      '@typescript-eslint/no-angle-bracket-type-assertion': 'warn',
-      '@typescript-eslint/no-array-constructor': 'warn',
-      '@typescript-eslint/no-namespace': 'error',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          args: 'none',
-          ignoreRestSiblings: true,
-        }
-      ]
+        'react/prop-types': 'off'
+      }
     }
-  }
   ]
 }
