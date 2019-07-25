4b584bc0deb2f97f9933e06afd9471a5d6c4d857
JoshAshby <joshuaashby@joshashby.com>
2019-07-25 08:58:21 -0600

    update and get admin roles controller specs passing

--- spec/controllers/admin/roles_controller_spec.rb
+++ spec/controllers/admin/roles_controller_spec.rb
@@ -26,113 +26,129 @@ require 'rails_helper'
 RSpec.describe Admin::RolesController, type: :controller do
 
   # This should return the minimal set of attributes required to create a valid
-  # Admin::Role. As you add validations to Admin::Role, be sure to
+  # Role. As you add validations to Role, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:valid_attributes) do
+    {
+      name: "test"
+    }
+  end
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:invalid_attributes) do
+    {
+      name: nil
+    }
+  end
+
+  let(:user) { create :user, :with_permissions, roles_and_permissions: { admin: [ 'admin.access' ] } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # Admin::RolesController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  let(:valid_session) do
+    {
+      user_id: user.id
+    }
+  end
 
   describe "GET #index" do
     it "returns a success response" do
-      role = Admin::Role.create! valid_attributes
+      Role.create! valid_attributes
       get :index, params: {}, session: valid_session
-      expect(response).to be_success
-    end
-  end
-
-  describe "GET #show" do
-    it "returns a success response" do
-      role = Admin::Role.create! valid_attributes
-      get :show, params: {id: role.to_param}, session: valid_session
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
 
   describe "GET #new" do
     it "returns a success response" do
       get :new, params: {}, session: valid_session
-      expect(response).to be_success
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns a success response" do
-      role = Admin::Role.create! valid_attributes
-      get :edit, params: {id: role.to_param}, session: valid_session
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
 
   describe "POST #create" do
     context "with valid params" do
-      it "creates a new Admin::Role" do
+      it "creates a new Role" do
+        user
         expect {
-          post :create, params: {admin_role: valid_attributes}, session: valid_session
-        }.to change(Admin::Role, :count).by(1)
+          post :create, params: {role: valid_attributes}, session: valid_session
+        }.to change(Role, :count).by(1)
       end
 
-      it "redirects to the created admin_role" do
-        post :create, params: {admin_role: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(Admin::Role.last)
+      it "redirects to the created role" do
+        post :create, params: {role: valid_attributes}, session: valid_session
+        expect(response).to redirect_to([:admin, Role.last])
       end
     end
 
     context "with invalid params" do
       it "returns a success response (i.e. to display the 'new' template)" do
-        post :create, params: {admin_role: invalid_attributes}, session: valid_session
-        expect(response).to be_success
+        post :create, params: {role: invalid_attributes}, session: valid_session
+        expect(response).to be_successful
       end
+    end
+  end
+
+  describe "GET #show" do
+    it "returns a success response" do
+      role = Role.create! valid_attributes
+      get :show, params: {id: role.to_param}, session: valid_session
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns a success response" do
+      role = Role.create! valid_attributes
+      get :edit, params: {id: role.to_param}, session: valid_session
+      expect(response).to be_successful
     end
   end
 
   describe "PUT #update" do
     context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+      let(:updated_name) { "test - updated" }
 
-      it "updates the requested admin_role" do
-        role = Admin::Role.create! valid_attributes
-        put :update, params: {id: role.to_param, admin_role: new_attributes}, session: valid_session
-        role.reload
-        skip("Add assertions for updated state")
+      let(:new_attributes) do
+        {
+          name: updated_name
+        }
       end
 
-      it "redirects to the admin_role" do
-        role = Admin::Role.create! valid_attributes
-        put :update, params: {id: role.to_param, admin_role: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(role)
+      it "updates the requested role" do
+        role = Role.create! valid_attributes
+        put :update, params: {id: role.to_param, role: new_attributes}, session: valid_session
+        role.reload
+        expect(role.name).to eq(updated_name)
+      end
+
+      it "redirects to the role" do
+        role = Role.create! valid_attributes
+        put :update, params: {id: role.to_param, role: valid_attributes}, session: valid_session
+        expect(response).to redirect_to(admin_role_url(role))
       end
     end
 
     context "with invalid params" do
       it "returns a success response (i.e. to display the 'edit' template)" do
-        role = Admin::Role.create! valid_attributes
-        put :update, params: {id: role.to_param, admin_role: invalid_attributes}, session: valid_session
-        expect(response).to be_success
+        role = Role.create! valid_attributes
+        put :update, params: {id: role.to_param, role: invalid_attributes}, session: valid_session
+        expect(response).to be_successful
       end
     end
   end
 
   describe "DELETE #destroy" do
-    it "destroys the requested admin_role" do
-      role = Admin::Role.create! valid_attributes
+    it "destroys the requested role" do
+      user
+      role = Role.create! valid_attributes
       expect {
         delete :destroy, params: {id: role.to_param}, session: valid_session
-      }.to change(Admin::Role, :count).by(-1)
+      }.to change(Role, :count).by(-1)
     end
 
-    it "redirects to the admin_roles list" do
-      role = Admin::Role.create! valid_attributes
+    it "redirects to the roles list" do
+      role = Role.create! valid_attributes
       delete :destroy, params: {id: role.to_param}, session: valid_session
       expect(response).to redirect_to(admin_roles_url)
     end
