301371e67c9aa3cafa675f85a30bd728d977a0d9
JoshAshby <joshuaashby@joshashby.com>
2019-07-25 09:21:11 -0600

    fillout but don't get admin roles requests tests

--- spec/requests/admin/admin_roles_spec.rb
+++ spec/requests/admin/admin_roles_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "Admin::Roles", type: :request do
-  describe "GET /admin_roles" do
-    it "works! (now write some real specs)" do
-      get admin_roles_path
-      expect(response).to have_http_status(200)
-    end
+# https://relishapp.com/rspec/rspec-rails/docs/request-specs/request-spec
+RSpec.describe "Admin::Roles management", type: :request do
+  before do
+    allow_any_instance_of(RoleConstraint).to receive(:matches?).and_return(true)
+  end
+
+  it "creates a new role and redirects to its page" do
+    skip "Requires a gem and i'm working offline"
+
+    get new_admin_role_path
+    expect(response).to render_template(:new)
+
+    post admin_roles_path, params: { role: { name: "test" } }
+
+    expect(response).to redirect_to(assigns(:role))
+    follow_redirect!
+
+    expect(response).to render_template(:show)
+    expect(response.body).to include("Role was successfully created.")
   end
 end
