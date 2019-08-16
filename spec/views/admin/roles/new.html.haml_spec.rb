af10fbc35f2b57f9485239e3bc51cfe1946e95c3
JoshAshby <joshuaashby@joshashby.com>
2019-08-16 10:41:31 -0600

    helps to assign the right type of data to the view test

--- spec/views/admin/roles/new.html.haml_spec.rb
+++ spec/views/admin/roles/new.html.haml_spec.rb
@@ -2,14 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "admin/roles/new", type: :view do
   before(:each) do
-    assign(:role, create(:role))
+    assign(:role, build(:role))
   end
 
   it "renders new role form" do
     render
 
-    assert_select "form[action=?][method=?]", admin_roles_path, "post" do
-      debugger
-    end
+    assert_select "form[action=?][method=?]", admin_roles_path, "post"
   end
 end
