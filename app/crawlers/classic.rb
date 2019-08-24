38b7ea3f207b5d241b7d62434753af7e58522da6
JoshAshby <joshuaashby@joshashby.com>
2019-08-23 22:01:24 -0600

    experiment with making an easier to maintain crawler
    while also thinking about the future of being able to produce WARC
    files and have a better set of logs and information around requests
    and responses during caching to aid in debugging
    
    and also thinking about extendability and introducing newer crawlers
    while keeping the same data structures

---
+++ app/crawlers/classic.rb
@@ -0,0 +1,205 @@
+require "tempfile"
+
+module HTTPRequestInResponse
+  refine HTTP::Response do
+    attr_accessor :req
+  end
+
+  refine HTTP::Client do
+    alias_method :__perform__, :perform
+
+    def perform(req, options)
+      __perform__(req, options).tap { |res| res.req = req }
+    end
+  end
+end
+
+using HTTPRequestInResponse
+
+Request = Struct.new(:uri, :method, :headers, keyword_init: true) do
+end
+
+Response = Struct.new(:status, :headers, :body, keyword_init: true) do
+end
+
+class Document
+  extend Forwardable
+
+  attr_accessor :uri, :request, :response, :content_type, :parent, :dependencies
+
+  def initialize uri
+    @uri = uri
+  end
+
+  def dependencies
+    @dependencies ||= {}
+  end
+
+  def address
+    @address ||= Addressable::URI.parse uri
+  end
+
+  # TODO: What if this is JSON and instead of xpath it's a json_path?
+  def nokogiri
+    @nokogiri ||= Nokogiri::HTML(response.body)
+  rescue => e
+    debugger
+  end
+
+  def_delegators :nokogiri, :xpath
+
+  # TODO: move this logic to a "dependency resolver/parser"
+  def asset_links uri
+    [
+      "//link[@rel='stylesheet']/@href",
+      "//script/@src",
+      "//img/@src"
+    ].flat_map(&nokogiri.method(:xpath))
+      .map(&:to_s)
+      .uniq
+      .compact
+      .map(&Addressable::URI.method(:parse))
+      .map { |link| (uri + link).to_s }
+  end
+
+  def add_dependency link, dep
+    dep.parent = self
+    dependencies[link] = dep
+  end
+end
+
+class Classic
+  class Client
+    extend Forwardable
+
+    def config
+      @config ||= ActiveSupport::OrderedOptions.new.tap do |opts|
+        opts.user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:57.0) Gecko/20100101 Firefox/57.0"
+
+        opts.headers = {
+          accept_language: "en-US,en;q=0.5",
+          accept: "text/html;q=0.9,*/*;q=0.8; charset=utf-8"
+        }
+
+        opts.follow_redirects = true
+      end
+    end
+
+    def_delegators :base, *HTTP::Request::METHODS
+
+    private
+
+    def base
+      @base ||= HTTP.headers({ user_agent: config.user_agent }.merge(config.headers))
+        .yield_self { |client| config.follow_redirects ? client.follow : client }
+    end
+  end
+
+  def crawl site
+    root = crawl_deps site
+
+    fetch_favicon_for root
+
+    return root
+  end
+
+  private
+
+  def crawl_deps site
+    root = request site
+
+    fetch_assets_for root
+
+    return root
+  end
+
+  def client
+    @client ||= Client.new
+  end
+
+  def fetch_favicon_for root
+    default_location = root.address.join("/favicon.ico").to_s
+    res = request default_location
+
+    root.add_dependency default_location, res
+    return if (200...299).include? res.response.status
+
+    root.xpath('//link[@rel="shortcut icon"]').find do |possible_favicon|
+      href = root.address.join(possible_favicon.attr("href")).to_s
+
+      res = request href
+
+      root.add_dependency href, res
+    end
+  end
+
+  def fetch_assets_for root
+    root.asset_links(root.address).each do |link|
+      dep = crawl_deps link
+
+      root.add_dependency link, dep
+    end
+  end
+
+  def request uri
+    document = Document.new uri
+
+    puts "Fetching uri: '#{ uri }'"
+
+    res = client.get uri
+
+    body, content_type = binary_temp_file do |temp_file|
+      write_body response: res, to: temp_file
+      content_type = get_content_type response: res, io: temp_file
+
+      [temp_file.read, content_type]
+    end
+
+    # TODO: There is a lot more info to capture from the request and
+    # response.
+    document.request = Request.new uri: uri, method: "GET", headers: {} #res.req.headers
+    document.response = Response.new status: res.code, headers: res.headers, body: body
+
+    document.content_type = content_type
+
+    return document
+  end
+
+  def binary_temp_file
+    Tempfile.create do |temp_file|
+      temp_file.binmode
+
+      yield temp_file
+    end
+  end
+
+  # Streams the response body into the temp file to hopefully avoid some
+  # memory issues if this is a large document
+  def write_body response:, to:
+    io_handle = to
+
+    response.body.each do |partial|
+      io_handle.write partial
+    end
+
+    io_handle.rewind
+  end
+
+  def get_content_type response:, io:
+    return response.content_type.mime_type if response.content_type.mime_type
+
+    io_handle = io
+    io_handle.rewind
+
+    # some websites *cough*offline.pink*cough* don't return a content type
+    # header, so we'll first see if the html doctype string is present and
+    # guess this is text/html or we'll fallback to assuming its binary
+    # https://en.wikipedia.org/wiki/Content_sniffing
+    content_type ||= MimeMagic.by_magic(io_handle)
+    content_type ||= "application/octet-stream"
+
+    io_handle.rewind
+
+    content_type
+  end
+end
