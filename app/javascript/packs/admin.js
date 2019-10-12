624748e46fa702b7a5ca2174bdffeaf673dded45
JoshAshby <joshuaashby@joshashby.com>
2019-10-11 19:21:54 -0600

    fix an issue with CSP and reactrailsujs
    build packs in tests?
    
    hmm
    
    move store
    
    remove action change

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -23,7 +23,26 @@ ActiveStorage.start()
 
 // Support component names relative to this directory:
 const componentRequireContext = require.context("components", true)
-ReactRailsUJS.useContext(componentRequireContext)
+// ReactRailsUJS.useContext(componentRequireContext)
+// Copy pasta from the react-rails ujs source to avoid CSP issues with eval
+ReactRailsUJS.getConstructor = className => {
+  const parts = className.split(".")
+  const filename = parts.shift()
+  const keys = parts
+
+  // Load the module:
+  let component = componentRequireContext("./" + filename)
+
+  // Then access each key:
+  keys.forEach(function(k) {
+    component = component[k]
+  })
+
+  // support `export default`
+  if (component.__esModule) component = component["default"]
+
+  return component
+}
 // ReactRailsUJS doesn't seem to pick up on Turbolinks like it should
 // I wonder if its because Turbolinks.start needs to be called before
 // importing ReactRailsUJS?
