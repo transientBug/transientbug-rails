2bf37bfe00af9537a7d1fa6805c12d01a61cd2d0
JoshAshby <joshuaashby@joshashby.com>
2020-12-19 00:11:21 -0700

    helper classes for a default button and input style that should fit 99% of needs. work on invite and session views a tad

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-  layout "page"
-
   before_action :redirect_home, only: [ :index, :new ], if: :signed_in?
 
   require_login! only: [ :destroy ]
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
-    require("postcss-import"),
+    require("postcss-easy-import"),
+    require("postcss-nested"),
     require("tailwindcss"),
     require("postcss-flexbugs-fixes"),
     require("postcss-preset-env")({
