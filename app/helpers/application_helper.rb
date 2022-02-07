2dc04695e14f1cb089e48c0a914a66db1c32eb91
JoshAshby <joshuaashby@joshashby.com>
2022-02-06 19:48:37 -0700

    rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def auth_path(provider, *args)= [ "auth", provider.to_s, args ].flatten.compact.join "/"
+  def auth_path(provider, *args)= [ "auth", provider.to_s, args ].flatten.compact.join("/")
 
   def template_path_for *args
     template_prefix = "public" unless controller_path.start_with? "admin/"
@@ -16,7 +16,7 @@ module ApplicationHelper
     ].concat(args).flatten.compact.join "/"
   end
 
-  def feather_svg(name, **opts)= inline_svg_pack_tag "feather/#{ name }.svg", **opts
+  def feather_svg(name, **opts)= inline_svg_pack_tag("feather/#{ name }.svg", **opts)
 
   def service_announcements
     service_announcements = ServiceAnnouncement.displayable
