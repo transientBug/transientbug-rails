1af90451865666aebb1f7fb5c282f9495b7f9d0c
JoshAshby <joshuaashby@joshashby.com>
2022-02-12 12:26:59 -0700

    remove a eslint thingy

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Entry point for the build script in your package.json
-import * as turbo from "@hotwired/turbo-rails"
+import from "@hotwired/turbo-rails"
 import * as ActionCable from "@rails/actioncable"
 
 ActionCable.logger.enabled = true
