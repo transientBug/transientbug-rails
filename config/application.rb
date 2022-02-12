0a240f6621e13b6b00179db89f22a7ec0cf7ab74
JoshAshby <joshuaashby@joshashby.com>
2022-02-12 13:31:04 -0700

    full-frame and frame options trial for the dashboards

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,20 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+class FrameOption
+  def initialize app
+    @app = app
+  end
+
+  def call env
+    status, headers, response = @app.call(env)
+
+    headers["X-Frame-Options"] = "SAMEORIGIN"
+
+    [status, headers, response]
+  end
+end
+
 module TransientBug
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -30,5 +44,7 @@ module TransientBug
 
     config.action_cable.mount_path = "/websocket"
     config.action_cable.allowed_request_origins = ["https://staging.transientbug.ninja", "https://transientbug.ninja"]
+
+    config.middleware.use FrameOption
   end
 end
