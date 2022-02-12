08fa6bc560b40cd944e23041466892bc9695eee6
JoshAshby <joshuaashby@joshashby.com>
2022-02-12 12:33:03 -0700

    i know how to remove things :|

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Entry point for the build script in your package.json
-import from "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails"
 import * as ActionCable from "@rails/actioncable"
 
 ActionCable.logger.enabled = true
