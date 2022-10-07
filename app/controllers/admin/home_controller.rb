39d538c0a407963ea0940b518f8347bd567f316c
JoshAshby <joshuaashby@joshashby.com>
2022-10-07 00:08:09 -0600

    counts for the admin dashboard of 7 day changes for users and total # of bookmarks
    mostly as a curiosity

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -2,7 +2,13 @@
 
 class Admin::HomeController < AdminController
   # GET /admin
-  def home; end
+  def home
+    @bookmarks_count = Bookmark.count
+    @bookmarks_7days_ago_count = Bookmark.where(Bookmark.arel_table[:created_at].lt(7.days.ago)).count
+
+    @users_count = User.count
+    @users_7days_ago_count = User.where(User.arel_table[:created_at].lt(7.days.ago)).count
+  end
 
   def logs; end
   def workers; end
